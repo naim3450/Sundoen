9612ad01ce36b9aa8db57b77d3b6e7dc2f2e063e
naim3450 <naim.saim74@gmail.com>
2024-12-04 20:26:05 +0600

    latest update

--- JS/script.js
+++ JS/script.js
@@ -25,3 +25,13 @@ function loaderAnimation() {
     }, 2800);
 }
 loaderAnimation()
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
